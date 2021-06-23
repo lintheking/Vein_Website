a9f50a7b6bf31f350f7b33bd8f274295f6464467
lintheking <alexander.lin@gmx.de>
2021-06-23 15:10:00 +0200

    twitter

--- assets/js/baumcounter.js
+++ assets/js/baumcounter.js
@@ -108,3 +108,23 @@ function showSlides(n) {
                 menuVisible = true;
             });
         });
+
+        $(function(){
+    
+          $('a[href*="#"]').click(function (e) {
+              var href = $(this).attr('href'),
+              $scrollAim = $(href);
+      
+              // Abbrechen wenn kein Sprungziel gefunden
+              if ($scrollAim.length == 0) return;
+      
+              // Automatisches Scrollen verhindern
+              e.preventDefault();
+      
+              // Zum Sprungziel animieren
+              $('html,body').animate({
+                  scrollTop: $scrollAim.offset().top
+              }, 700);
+          });
+      
+      });
