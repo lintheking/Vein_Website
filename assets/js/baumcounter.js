e479d84b30e8a594a99b2a7b06371962394a7794
lintheking <alexander.lin@gmx.de>
2021-06-28 23:06:21 +0200

    gott

--- assets/js/baumcounter.js
+++ assets/js/baumcounter.js
@@ -18,6 +18,22 @@ $(function () {
 
 });
 
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
+
 setInterval(function () {
   // doFlip(3);
 }, 1000);
